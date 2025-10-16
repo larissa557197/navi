921c0f6791c5343aebbefd1766f6d69275ec2d76
larissa557197 <larissampmuniz@gmail.com>
2025-10-16 09:47:49 -0300

    refacto main

--- src/main/java/br/com/fiap/naviia/NaviIaApplication.java
+++ src/main/java/br/com/fiap/naviia/NaviIaApplication.java
@@ -7,7 +7,7 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 
 @SpringBootApplication
 @Theme(value = "my-theme", variant = "dark")
-public class NaviIaApplication implements AppShellConfigurator
+public class NaviIaApplication implements AppShellConfigurator{
     public static void main(String[] args) {
         SpringApplication.run(NaviIaApplication.class, args);
     }
